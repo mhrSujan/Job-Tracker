b56e4f405de119e332e40a741ab31b0401c75ca3
mhrSujan <sujanmaharjan054@gmail.com>
2026-04-26 14:33:50 -0400

    FrontEnd and Database connection

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = function (req, res, next) {
+module.exports = (req, res, next) => {
   const token = req.headers.authorization;
+
   if (!token) return res.status(401).json("No token");
 
   try {
@@ -9,6 +10,6 @@ module.exports = function (req, res, next) {
     req.user = decoded;
     next();
   } catch {
-    res.status(401).json("Invalid token");
+    return res.status(401).json("Invalid token");
   }
 };
